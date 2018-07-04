042b9d8e6bf63afdaa307dae19e4e7b613472b88
Neha Varshney <nehavarshney@QAIT.COM>
2018-07-04 16:49:03 +0530

    Initial commit

---
+++ Pattern1.java
@@ -0,0 +1,77 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args)
+	{
+//		-------------------------- 1 -----------------------------
+		/*
+		  	output-	****
+					****
+					****
+					****
+
+		  for(int i=1;i<=4;i++)
+		 
+			{
+				for(int j=1;j<=4;j++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}*/
+/*			---------------------------- 2 ------------------------------
+		
+ 					*
+					**
+					***
+					****	
+		 
+		for(int i=1;i<=4;i++)
+		{										
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}			
+				System.out.println();
+		}
+		*/
+/*			----------------------------- 3 --------------------------------
+		****
+		*  *
+		*  *
+		****	
+
+		for(int i=1;i<=4;i++)
+		{										
+			for(int j=1;j<=4;j++)
+			{
+				if(i==4 || j==4 || i==1 || j==1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}			
+			System.out.println();
+		}
+		*/
+		
+/*			---------------------------- 4 -------------------------------
+		
+		****
+		***
+		**
+		*
+
+		
+		for(int i=4;i>0;i--)
+		{										
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");		
+			}
+			System.out.println();
+		}
+	
+		*/
+	
+	}
+}
